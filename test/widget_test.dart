2bb8a4d4035b8f09e46fc12fe313701444e6473c
Jayanta <bjayanta.me@gmail.com>
2021-12-19 01:55:38 +0600

    init branch

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:auth/main.dart';
+import 'package:trip_app_ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
